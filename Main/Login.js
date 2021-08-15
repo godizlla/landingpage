b881a21664cf3ee55d8ce9d30d14047e20b4ddc5
godizlla <licksomebooty@gmail.com>
2021-08-15 15:21:23 -0700

    Add files via upload

---
+++ Main/Login.js
@@ -0,0 +1,25 @@
+//login page
+function checkCreds(){
+    //check if started or not
+    console.log("Checking has Started")
+    //grabbing input from hmtl and make it to var for java
+    var fristname = document.getElementById("fName").value;
+    var lastname = document.getElementById("lName").value;
+    var codenumber = document.getElementById("codeID").value;
+
+    var fullname = fristname + " " + lastname;
+
+    //Shows that all inputs are made
+    console.log("Fist name is: " + fristname + ", last name is: " + lastname + ", and code ID is: " + codenumber);
+    console.log("Full name " + fullname)
+
+    if(fullname.length > 20){
+        document.getElementById("loginStaus").innerHTML = "Full name has too many characters! Please try again..."
+    }else if (codenumber > 999){
+        document.getElementById("loginStaus").innerHTML = "Invaded Code"
+    }else {
+        alert("Welcome Back " + fullname);
+        location.replace("../main.html");
+    }
+
+}
---
+++ Main/dice.js
@@ -0,0 +1,36 @@
+/* Dice Roll  */
+
+function playcraps(){
+    //For the consloe to log
+    console.log("Craps Game Started")
+    // To Id the two diffent Dice
+    var die1 = Math.ceil(Math.random() * 6);
+    var die2 = Math.ceil(Math.random() * 6);
+    //Aduio For winning at craps 
+    var win = new Audio("../Main/Final Fantasy VII - Victory.mp3");
+    var tie = new Audio("../Main/lose.mp3");
+    //Replaces the elements on the html file with the dice numbers
+    document.getElementById("die1Res").innerHTML = die1;
+    document.getElementById("die2Res").innerHTML = die2;
+    //Adds the two dice number
+    var dieSum = die1 + die2;
+    //Replaces the elements on the html file with teh dice total
+    document.getElementById("diceSum").innerHTML = dieSum;
+    //debates if you win or lose 
+    if(dieSum == 7 || dieSum == 11){
+        //Replaces the elements on the html file with the lose condition
+        document.getElementById("crapsStatus").innerHTML = "Craps! You Lose";
+        tie.play();
+        //Replaces the elements on the html file with the win condition 
+    }else if (die1 == die2 && die1 % 2 ==0){
+        document.getElementById("crapsStatus").innerHTML = "Double You Win" ;
+        //Aduio For winning at craps 
+        win.play();
+
+    }else {
+        ////Replaces the elements on the html file with retry condition
+        document.getElementById("crapsStatus").innerHTML = "You didn't win or lose roll again ";
+    
+    }
+
+}
